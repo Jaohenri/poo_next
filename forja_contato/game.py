a8b3588fea79209d2009dc236ab8833c10dadb30
Jaohenri <joaohrcf10@gmail.com>
2025-12-07 21:45:19 -0300

    Switching Human names

---
+++ forja_contato/game.py
@@ -0,0 +1,8 @@
+class Game:
+    def __init__ (self,name, synopsis, genre, platform, status):
+        self.name = name
+        self.synopsis = synopsis
+        self.genre = genre
+        self.platform = platform
+        self.status = status
+        pass
