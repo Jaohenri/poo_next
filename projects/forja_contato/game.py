4c6c62f156ccc02d2be3d5044bfc3725c2d704d7
Jaohenri <joaohrcf10@gmail.com>
2025-12-14 18:09:42 -0300

    Creating a base class Entitity for the derivated classes Game and Person

--- projects/forja_contato/game.py
+++ projects/forja_contato/game.py
@@ -1,15 +1,31 @@
-class Game:
-    def __init__ (self,name, synopsis, genre, platform,engine, status):
+class Entity:
+    def __init__(self, name):
         self.name = name
-        self.synopsis = synopsis
-        self.genre = genre
-        self.platform = platform
-        self.engine = engine
-        self.status = status
         self.active = True
 
     def set_active(self,active):
         self.active = active
 
     def is_active(self):
-        return self.active
+        return self.active      
+
+class Game(Entity):
+
+    def __init__(self, name, synopsis, genre, platform,engine, status):
+        super().__init__(name)
+        self.synopsis = synopsis
+        self.genre = genre
+        self.platform = platform
+        self.engine = engine
+        self.status = status
+
+class Person(Entity):
+    def __init__(self, name, email, address, position):
+        super().__init__(name)
+        self.email = email
+        self.address = address
+        self.position = position
+
+
+
+   
