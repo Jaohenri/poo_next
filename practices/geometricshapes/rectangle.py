f206b9ec3bc087a8841b7246b9c3313c02577158
Jaohenri <joaohrcf10@gmail.com>
2025-12-23 21:33:45 -0300

    Retangle.py documentation

--- practices/geometricshapes/rectangle.py
+++ practices/geometricshapes/rectangle.py
@@ -1,6 +1,15 @@
+"""Module for Rectangle"""
 from geometric_shape import GeometricShape
 
 class Rectangle(GeometricShape):
+    """
+    Represents a Rectangle
+    
+    Methods:
+    calculate_area -> Float: Calculate the Area of the rectangle.
+    calculate_perimeter -> float: Calculate the perimeter of the rectangle.
+    details -> str: returns the rectangle details.
+    """
     def __init__(self, width: float, height: float) -> None:
         if width <= 0 or height <= 0:
             raise ValueError('Width and Height cannot have values less than 0')
@@ -8,10 +17,19 @@ class Rectangle(GeometricShape):
         self.height = height
 
     def calculate_area(self) -> float:
+        """
+        Calculate the Area of the rectangle.
+        """
         return self.height * self.width
-    
+
     def calculate_perimeter(self) -> float:
+        """
+        Calculate the perimeter of the rectangle.
+        """
         return 2 * (self.width + self.height)
-    
+
     def details(self) -> str:
+        """
+        Returns details of the rectangle.
+        """
         return f"Rectangle - Width: {self.width}, Height: {self.height}"
