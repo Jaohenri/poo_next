4e68fd5a9657d72f153a2d2d96b46ea5878cf096
Jaohenri <joaohrcf10@gmail.com>
2025-12-23 22:35:13 -0300

    Removing projects to create especifics repository to them

--- projects/polimorfic_adventure/character.py
+++
@@ -1,8 +0,0 @@
-from entity import Entity
-
-class Character(Entity):
-    def special_ability(self) -> float:
-        damage = self.strength * 2
-        print(f'\n{self.name} has attacked using a special ability causing {damage} damage.\n')
-        return damage
-    
