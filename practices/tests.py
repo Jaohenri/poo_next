c59d009701746bc88a3ff9a72861afe45c9a1a70
Jaohenri <joaohrcf10@gmail.com>
2026-01-28 22:34:50 -0300

    Adding exception test case

--- practices/tests.py
+++ practices/tests.py
@@ -1,18 +1,25 @@
 import unittest
 
-def sum(n1: int, n2: int) -> int:
+def sum_numbers(n1: int, n2: int) -> int:
     return n1 + n2
+
+def divide_numbers(n1: int, n2: int) -> int:
+    return n1 / n2
 
 class SumTest(unittest.TestCase):
     
     def test_simple_sum(self):
-        result = sum(5,5)
+        result = sum_numbers(5,5)
         self.assertEqual(result, 10)
 
     def test_negative_sum(self):
-        result = sum(-10, -20)
+        result = sum_numbers(-10, -20)
         self.assertEqual(result, -30)
 
     def test_pos_negative_sum(self):
-        result = sum(10, -8)
+        result = sum_numbers(10, -8)
         self.assertEqual(result, 2)
+
+    def test_raise_exception(self):
+        with self.assertRaises(ZeroDivisionError):
+            divide_numbers(10, 0)
