afeb39bd8937f5fda1af36dc43950968715c64f6
Jaohenri <joaohrcf10@gmail.com>
2026-01-28 22:23:34 -0300

    Adding more test cases

--- practices/tests.py
+++ practices/tests.py
@@ -8,3 +8,11 @@ class SumTest(unittest.TestCase):
     def test_simple_sum(self):
         result = sum(5,5)
         self.assertEqual(result, 10)
+
+    def test_negative_sum(self):
+        result = sum(-10, -20)
+        self.assertEqual(result, -30)
+
+    def test_pos_negative_sum(self):
+        result = sum(10, -8)
+        self.assertEqual(result, 2)
