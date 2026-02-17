92a13ed00b13899f1ac426d9ab86f91009c307c8
Jaohenri <joaohrcf10@gmail.com>
2026-02-16 21:26:20 -0300

    Creating testes for tasks

---
+++ practices/test_tasks.py
@@ -0,0 +1,45 @@
+import unittest
+from tasks import TaskManagementSystem
+
+class TestTaskManagementSystem(unittest.TestCase):
+    def test_id_add_task(self):
+        tasks = TaskManagementSystem()
+        id_task1 = tasks.add_task("Clean the dishes")
+        id_task2 = tasks.add_task("Study Python")
+
+        self.assertEqual(id_task1, 1)
+        self.assertEqual(id_task2, 2)
+
+    def test_add_task_exception(self):
+        with self.assertRaises(ValueError):
+            tasks = TaskManagementSystem()
+            tasks.add_task("")
+
+    def test_list_tasks(self):
+        tasks = TaskManagementSystem()
+        id_task1 = tasks.add_task("Clean the dishes")
+        tasks_list = tasks.list_tasks()
+
+        self.assertEqual(tasks_list[0]["id"], id_task1)
+        self.assertEqual(tasks_list[0]["description"],"Clean the dishes")
+        self.assertFalse(tasks_list[0]["completed"])
+
+    def test_complete_task(self):
+        tasks = TaskManagementSystem()
+        id_task1 = tasks.add_task("Clean the dishes")
+        tasks.complete_task(id_task1)
+        tasks_list = tasks.list_tasks()
+
+
+        self.assertTrue(tasks_list[0]["completed"])
+
+    def test_remove_task(self):
+        tasks = TaskManagementSystem()
+        id_task1 = tasks.add_task("Clean the dishes")
+        tasks.remove_task(id_task1)
+        tasks_list = tasks.list_tasks()
+        task_ids = [task["id"] for task in tasks_list]
+        self.assertNotIn(id_task1, task_ids)
+
+if __name__ == "__main__":
+    unittest.main()
